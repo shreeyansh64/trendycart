9590a6d694e87bc7a51776aa4412708877bb71a1
shreeyansh64 <shreeyansh64@gmail.com>
2025-10-03 12:14:57 +0530

    CartPage: Created AppBar

--- lib/pages/cartpage.dart
+++ lib/pages/cartpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -11,7 +12,34 @@ class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("CartPage",style: TextStyle(color: Colors.black),),),
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(left: 190),
+          child: Text(
+            "My bag (1)",
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(FontAwesomeIcons.penToSquare),
+            iconSize: 25,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(FontAwesomeIcons.heart),
+              iconSize: 25,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
