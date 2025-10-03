a75041e5eedd25401b3863d41691ab15d12023c1
shreeyansh64 <shreeyansh64@gmail.com>
2025-10-03 14:00:19 +0530

    Completed CartPage

--- lib/pages/cartpage.dart
+++ lib/pages/cartpage.dart
@@ -40,218 +40,264 @@ class _CartPageState extends State<CartPage> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 20, bottom: 30),
-            child: Container(
-              color: Colors.grey[200],
-              height: 100,
-              width: double.infinity,
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: Text.rich(
-                    TextSpan(
-                      text: "Hooray! You're just ",
-                      style: TextStyle(color: Colors.black),
-                      children: [
-                        TextSpan(
-                          text: "₹990",
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text:
-                              " away from unlocking free shipping! Shipping charges on the below order is ",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        TextSpan(
-                          text: "₹99.",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 30),
+              child: Container(
+                color: Colors.grey[200],
+                height: 100,
+                width: double.infinity,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Hooray! You're just ",
+                        style: TextStyle(color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: "₹990",
+                            style: TextStyle(fontWeight: FontWeight.bold),
                           ),
-                        ),
-                      ],
+                          TextSpan(
+                            text:
+                                " away from unlocking free shipping! Shipping charges on the below order is ",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          TextSpan(
+                            text: "₹99.",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 250,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Image.asset(
-                  'assets/images/trousers.webp',
-                  height: 250,
-                  width: 250,
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+            Container(
+              width: double.infinity,
+              height: 250,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    'assets/images/trousers.webp',
+                    height: 250,
+                    width: 250,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Essential Beige Flat-Front Pants",
+                              style: TextStyle(color: Colors.black, fontSize: 15),
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Color.fromARGB(255, 235, 226, 202),
+                                  ),
+                                ),
+                                Text(
+                                  " / M(M)",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_down_outlined,
+                                  color: Colors.black,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 80,
+                        // color: Colors.red,
+                        child: Column(
+                          children: [
+                            Text(
+                              "₹1,490",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                            Container(
+                              height: 26,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: Color.fromARGB(255, 252, 240, 207),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  "Trendy",
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 94, 79, 44),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
                         children: [
+                          OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(horizontal: 5)
+                            ),
+                            onPressed: () {}, child: Text("x1")),
+                            SizedBox(width: 40),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.amber[300],
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              "CHECKOUT",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 40),
+              child: Container(
+                height: 150,
+                width: double.infinity,
+                color: Colors.grey[200],
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(FontAwesomeIcons.truckFast, size: 25),
+                          SizedBox(width: 20),
                           Text(
-                            "Essential Beige Flat-Front Pants",
+                            "Free Shipping for orders above ₹990",
                             style: TextStyle(color: Colors.black, fontSize: 15),
                           ),
-                          Row(
-                            children: [
-                              Container(
-                                height: 20,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  color: Color.fromARGB(255, 235, 226, 202),
-                                ),
-                              ),
-                              Text(
-                                " / M(M)",
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Icon(
-                                Icons.keyboard_arrow_down_outlined,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
                         ],
                       ),
-                    ),
-                    Container(
-                      height: 50,
-                      width: 80,
-                      // color: Colors.red,
-                      child: Column(
+                      Row(
                         children: [
+                          Icon(FontAwesomeIcons.creditCard, size: 25),
+                          SizedBox(width: 20),
                           Text(
-                            "₹1,490",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            ),
-                          ),
-                          SizedBox(width: 10),
-                          Container(
-                            height: 26,
-                            width: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(5),
-                              color: Color.fromARGB(255, 252, 240, 207),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Trendy",
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 94, 79, 44),
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
+                            "Secured Payment & Checkout",
+                            style: TextStyle(color: Colors.black, fontSize: 15),
                           ),
                         ],
                       ),
-                    ),
-                    Row(
-                      children: [
-                        OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            padding: EdgeInsets.symmetric(horizontal: 5)
+                      Row(
+                        children: [
+                          Icon(FontAwesomeIcons.rotate, size: 25),
+                          SizedBox(width: 20),
+                          Text(
+                            "Easy Returns, Free Pick Up",
+                            style: TextStyle(color: Colors.black, fontSize: 15),
                           ),
-                          onPressed: () {}, child: Text("x1")),
-                          SizedBox(width: 40),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.amber[300],
-                            padding: EdgeInsets.symmetric(horizontal: 20),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: Text(
-                            "CHECKOUT",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, bottom: 30),
+              child: Text(
+                "You might also like",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/images/polo.webp',width: 230,height: 250,fit: BoxFit.cover,),
+                      Text("Heritage Cotton Polo",style: TextStyle(color: Colors.black),),
+                      
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8,bottom: 3),
+                        child: Text("₹1,590",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+                      ),
+                      Text.rich(TextSpan(
+                        text: "Get it for ",
+                        style: TextStyle(color: Colors.green[400]),
+                        children:[TextSpan(text: "₹1,393",style: TextStyle(color: Colors.green[400],fontWeight: FontWeight.bold))]
+                      ))
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/images/polo2.webp',width: 230,height: 250,fit: BoxFit.cover,),
+                      Text("Élite Cotton Polo Tee",style: TextStyle(color: Colors.black),),
+                      
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8,bottom: 3),
+                        child: Text("₹1,390",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),),
+                      ),
+                      Text.rich(TextSpan(
+                        text: "Get it for ",
+                        style: TextStyle(color: Colors.green[400]),
+                        children:[TextSpan(text: "₹1,043",style: TextStyle(color: Colors.green[400],fontWeight: FontWeight.bold))]
+                      ))
+                    ],
+                  ),
                 ),
               ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40),
-            child: Container(
-              height: 150,
-              width: double.infinity,
-              color: Colors.grey[200],
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(FontAwesomeIcons.truckFast, size: 25),
-                        SizedBox(width: 20),
-                        Text(
-                          "Free Shipping for orders above ₹990",
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(FontAwesomeIcons.creditCard, size: 25),
-                        SizedBox(width: 20),
-                        Text(
-                          "Secured Payment & Checkout",
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(FontAwesomeIcons.rotate, size: 25),
-                        SizedBox(width: 20),
-                        Text(
-                          "Easy Returns, Free Pick Up",
-                          style: TextStyle(color: Colors.black, fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16, bottom: 30),
-            child: Text(
-              "You might also like",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
-            ),
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
